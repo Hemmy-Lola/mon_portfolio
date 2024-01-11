b9bb09808825f31e38d6c187b38c22d6a4dffa39
hemmy-lola <hl.mathys@gmail.com>
2024-01-11 14:06:35 +0100

    modification responsive

--- mon_portfolio/script.js
+++ mon_portfolio/script.js
@@ -15,3 +15,33 @@ gallery.forEach(image => {
         });
     });
 });
+
+let MenuBurgerButton = document.getElementById("menu-burger-button");
+let MenuBurger = document.getElementById("menu-burger");
+let MenuBurgerClose = document.getElementById("menu-burger-close");
+let OpenBurger = document.getElementById("open-burger");
+let Link = document.querySelectorAll(".link")
+
+MenuBurgerButton.addEventListener("click", function () {
+    if (MenuBurger.style.display === "none" || MenuBurger.style.display === "") {
+        MenuBurger.style.display = "block";
+    } else {
+        MenuBurger.style.display = "none";
+    }
+});
+
+MenuBurgerClose.addEventListener("click", function () {
+    MenuBurger.style.display = "none";
+});
+
+Link.forEach(element => {
+    element.addEventListener("click", function () {
+        MenuBurger.style.display = "none";
+    })
+});
+
+window.addEventListener("resize", function () {
+    if (window.screen > "770px") {
+        MenuBurger.style.display = "none";
+    }
+})
